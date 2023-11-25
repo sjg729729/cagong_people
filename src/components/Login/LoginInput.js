b632fab3564826438cb162e20b53d4648fc58faa
Jinjeon Shin <sjg729729@gmail.com>
2023-11-25 13:45:30 +0900

    feat: Add save user info (Login)

--- src/components/Login/LoginInput.js
+++ src/components/Login/LoginInput.js
@@ -1,35 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { IsLoginContext } from '../../context/IsLoginProvider';
 
-function LoginInput() {
+function LoginInput({ userInputHander }) {
   const { setIsLogin } = useContext(IsLoginContext);
+
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    department: '',
+    studentId: '',
+  });
+
+  const changeHandler = (value, action) => {
+    const newUserInfo = { ...userInfo };
+    newUserInfo[action] = value;
+    setUserInfo(newUserInfo);
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
+    window.sessionStorage.setItem('userInfo', JSON.stringify(userInfo));
     setIsLogin(true);
   };
 
   return (
     <form className="flex flex-col justify-center" onSubmit={submitHandler}>
       <div className="text-center my-3">
-        <label className="mr-2">이름</label>
+        <label htmlFor="name" className="mr-2">
+          이름
+        </label>
         <input
+          id="name"
           type="text"
           className="border border-solid border-blue-400 rounded-lg"
+          value={userInfo.name}
+          onChange={(e) => changeHandler(e.target.value, 'name')}
         ></input>
       </div>
       <div className="text-center my-3">
-        <label className="mr-2">학과</label>
+        <label htmlFor="department" className="mr-2">
+          학과
+        </label>
         <input
+          id="department"
           type="text"
           className="border border-solid border-blue-400 rounded-lg"
+          value={userInfo.department}
+          onChange={(e) => changeHandler(e.target.value, 'department')}
         ></input>
       </div>
       <div className="text-center my-3">
-        <label className="mr-2">학번</label>
+        <label htmlFor="studentId" className="mr-2">
+          학번
+        </label>
         <input
+          id="studentId"
           type="number"
           className="border border-solid border-blue-400 rounded-lg"
+          value={userInfo.studentId}
+          onChange={(e) => changeHandler(e.target.value, 'studentId')}
         ></input>
       </div>
       <div className="text-center my-3">
